dc6d2ebcf44f0f3a5b2d4f426a4be934770795b0
cyj2825 <cyj2825@naver.com>
2022-12-01 23:24:24 +0900

    대타근무 요청 상세 화면 구현 완료

---
+++ app/src/main/java/com/example/coalba/SubstituteDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coalba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubstituteDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_substitute_detail)
+    }
+}
