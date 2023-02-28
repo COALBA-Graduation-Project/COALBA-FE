45fffb44b33237344b51407e6388eac70b55f01a
cyj2825 <cyj2825@naver.com>
2023-02-28 11:46:48 +0900

    feat: 프로필 등록&수정 시 갤러리에서 이미지 선택 구현

--- app/src/main/java/com/example/coalba/ProfileRegisterActivity.kt
+++ app/src/main/java/com/example/coalba/ProfileRegisterActivity.kt
@@ -1,8 +1,26 @@
 package com.example.coalba
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.coalba.databinding.ActivityProfileRegisterBinding
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.File
 
 class ProfileRegisterActivity : AppCompatActivity() {
 
@@ -10,12 +28,79 @@ class ProfileRegisterActivity : AppCompatActivity() {
     private var mBinding: ActivityProfileRegisterBinding? = null
     // 매번 null 체크를 할 필요없이 편의성을 위해 바인딩 변수 재선언
     private val binding get() = mBinding!!
+    // 갤러리에서 가져온 프로필 이미지를 백 서버에게 보내주기 위함
+    var imageFile : File? = null
+    var imageWideUri: Uri? = null
+
+    companion object {
+        // 갤러리 권한 요청
+        const val REQ_GALLERY = 1
+    }
+
+    // 이미지를 결과값으로 받는 변수
+    private val imageResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode == RESULT_OK){
+            // 이미지를 받으면 ImageView에 적용
+            val imageUri = result.data?.data
+            imageWideUri = imageUri
+            imageUri?.let{
+                // 서버 업로드를 위해 파일 형태로 변환
+                // imageFile = File(getRealPathFromURI(it))
+
+                // 이미지를 불러온다
+                Glide.with(this)
+                    .load(imageUri)
+                    .fitCenter()
+                    .apply(RequestOptions().override(500,500))
+                    .into(binding.ivRegisterProfile)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 바인딩
         mBinding = ActivityProfileRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.ivRegisterCamera.setOnClickListener {
+            selectGallery()
+        }
+    }
+    // 이미지 실제 경로 반환
+    fun getRealPathFromURI(uri: Uri): String {
+        val buildName = Build.MANUFACTURER
+        if(buildName.equals("Xiaomi")){
+            return uri.path!!
+        }
+        var columnIndex = 0
+        val proj = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = contentResolver.query(uri, proj, null,null,null)
+        if(cursor!!.moveToFirst()){
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+        val result = cursor.getString(columnIndex)
+        cursor.close()
+        return result
+    }
+    // 갤러리를 부르는 메서드
+    private fun selectGallery(){
+        val writePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val readPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        // 권한 확인
+        if(writePermission == PackageManager.PERMISSION_DENIED || readPermission == PackageManager.PERMISSION_DENIED){
+            // 권한 요청
+            requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), REQ_GALLERY)
+        }else{
+            // 권한이 있는 경우 갤러리 실행
+            val intent = Intent(Intent.ACTION_PICK)
+            // intent의 data와 type을 동시에 설정하는 메서드
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*"
+            )
+            imageResult.launch(intent)
+        }
     }
 
     // 액티비티가 파괴될 때..
